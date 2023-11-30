16a890148c7500b8b4a851438c5c2690743f07c3
Will Bohlke <willbohlke@gmail.com>
2023-11-29 22:57:11 -0500

    modal works but there is a bug

--- public/js/application.js
+++ public/js/application.js
@@ -8,9 +8,34 @@ function handleButtonClick(id) {
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             json = JSON.parse(xhttp.responseText);
-
             character = json[id];
-            changeContent(character);
+
+            var modal = document.createElement('div');
+            modal.setAttribute('class', 'modal fade');
+            modal.setAttribute('id', 'characterModal');
+            modal.setAttribute('tabindex', '-1');
+            modal.setAttribute('aria-labelledby', 'characterModalLabel');
+            modal.setAttribute('aria-hidden', 'true');
+            modal.innerHTML = `
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="characterModalLabel">${character.name}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ${character.desc}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+            document.querySelector('.container').appendChild(modal);
+
+            var myModal = new bootstrap.Modal(document.getElementById('characterModal'), {});
+            myModal.show();
         }
     };
     xhttp.open("GET", "/api/character/", true);
@@ -18,7 +43,7 @@ function handleButtonClick(id) {
 }
 
 function changeContent(character) {
-    let element = document.querySelector('#content');
+    let element = document.createElement('div');
     element.innerHTML = `
         <h2>${character.name}</h2>
         <p>${character.desc}</p>
@@ -33,11 +58,20 @@ function createButtons() {
             var json = JSON.parse(xhttp.responseText);
             for (var i = 0; i < json.length; i++) {
                 var card = document.createElement("div");
+                var img = document.createElement("img");
+
+                document.querySelector('.container').appendChild(card);
                 card.setAttribute("class", "card");
                 card.setAttribute("style", "width: 18rem;");
+                
+                img.setAttribute("class", "card-img-top");
+                img.setAttribute("src", json[i].image);
+                
                 card.textContent = json[i].name;
-                //button.setAttribute("onclick", `handleButtonClick(${i})`);
-                document.querySelector('.container').appendChild(card);
+                
+                
+                card.appendChild(img);
+                card.setAttribute("onclick", `handleButtonClick(${i})`);
             }
             
         }
